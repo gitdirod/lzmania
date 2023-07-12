d3cf357e126837a5e062987871ef2688030ec2bc
Diego Rodriguez <git@dirod-ecuador.com>
2023-07-12 12:11:35 -0500

    se agregan api a groups

--- routes/api.php
+++ routes/api.php
@@ -133,9 +133,9 @@ Route::get('/type_products/{type_product}', [TypeProductController::class, 'show
 Route::middleware('auth:sanctum')->put('/type_products/{type_product}', [TypeProductController::class, 'update']);
 
 // Groups
-Route::get('/groups', [GroupController::class, 'index']);
-Route::post('/groups', [GroupController::class, 'store']);
-Route::get('/groups/{group}', [GroupController::class, 'show']);
+Route::middleware('auth:sanctum')->get('/groups', [GroupController::class, 'index']);
+Route::middleware('auth:sanctum')->post('/groups', [GroupController::class, 'store']);
+Route::middleware('auth:sanctum')->get('/groups/{group}', [GroupController::class, 'show']);
 Route::middleware('auth:sanctum')->put('/groups/{group}', [GroupController::class, 'update']);
 
 // Groups
