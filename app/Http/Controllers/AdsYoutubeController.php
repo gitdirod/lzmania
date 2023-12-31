86bd875fa3f21382c1a9d1a7822cf6b1c8328791
gitdirod <git@dirod-ecuador.com>
2023-12-30 20:19:21 -0500

    Se crea modelo controlador y migracion con el comando sail artisan make:model AdsYoutube --api --migration

---
+++ app/Http/Controllers/AdsYoutubeController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\AdsYoutube;
+use Illuminate\Http\Request;
+
+class AdsYoutubeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\AdsYoutube  $adsYoutube
+     * @return \Illuminate\Http\Response
+     */
+    public function show(AdsYoutube $adsYoutube)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\AdsYoutube  $adsYoutube
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, AdsYoutube $adsYoutube)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\AdsYoutube  $adsYoutube
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(AdsYoutube $adsYoutube)
+    {
+        //
+    }
+}
